e9944137775338489f111bf60adf9f87f6e8530f
Arslanishe <arslanishe553@gmail.com>
2021-03-18 18:00:48 +0300

    commit 4

--- Script.js
+++ Script.js
@@ -1,1 +1,14 @@
-console.log('hello js');
+1. console.log('hello js');
+
+2. console.log('hello js');
+
+3. console.log('hello js');
+
+
+4. console.log('hello js');
+5. console.log('hello js');
+6. console.log('hello js');
+
+12.59 console.log('hello js');
+
+13.00 console.log('hello js');
