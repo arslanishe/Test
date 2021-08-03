3ddfd87f9038b73d8ceef8b1ba140761a9fe2a6b
Arslanishe <arslanishe553@gmail.com>
2021-08-03 16:03:27 +0300

    Js go

---
+++ comm.js
@@ -0,0 +1,13 @@
+var str = "Че за ты анаконда ииииууууу",
+  number = 1,
+  numberSecond = 2.4,
+  bool = true,
+  bul = false,
+  undef,
+  nuull = null,
+  obj = {
+    prop: "yo",
+    prop: function name(params) {},
+  };
+
+alert(numberSecond);
