d98ba44b8bd63a2dbe7ba46da6911ed9ae794668
Arslanishe <arslanishe553@gmail.com>
2022-03-13 17:16:09 +0300

    go

--- python.py
+++ python.py
@@ -1,11 +1,8 @@
 def TestCode(a):
-    q = 0
     for i in range(len(a) - 1):
         for x in range(len(a)-1 -i):
-            print(f"сравниваем {a[x]} c {a[x+1]}")
-            q += 1
             if a[x] > a[x + 1]:
                 a[x],a[x+1] = a[x+1],a[x]
-    return a, q
+    return a
 
-print(TestCode([1,4,2,10,3,6,8,4,9,5]))
+print(*TestCode([1,4,2,10,3,6,8,4,9,5]))
